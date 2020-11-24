87321cfd6d00097a9cbe1b1aa20eab3139a65c3b
TheDryduck <53495631+TheDryduck@users.noreply.github.com>
2020-11-24 10:30:35 +0100

    Add files via upload

---
+++ MongoDB TEST/MongoDB TEST/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Animal
+{
+	public string name;
+	public string age;
+	public string happiness;
+    public string type;
+		
+	public Animal(string _name, string _age, string _happiness, string _type)
+    {
+		name = _name;
+		age = _age;
+		happiness = _happiness;
+        type = _type;
+    }
+}
